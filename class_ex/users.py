27a3ee57444f7f3950ec0fed55fcefc051e1f93a
AttackThwarter <attackthwarter@gmail.com>
2024-12-04 22:36:25 +0330

    add Inheritance and SelerAndBuyer class in user.py

--- class_ex/users.py
+++ class_ex/users.py
@@ -19,7 +19,7 @@ class UserList(list["User"]):
 class User:
 
     all_users: list["User"] = UserList()
-    def __init__(self, user_name: str, email: str, password: str) -> None:
+    def __init__(self, user_name: str, email: str, password: str, **kwargs) -> None:
         
         self.user_name = user_name
         self.email = email
@@ -34,16 +34,28 @@ class User:
         return f"{self.user_name}"
     
 
-class Seller(User):
+class Order:
+    pass
 
-    def order(self, order: "Order") -> None:
+
+class Seller(User):
+    def __init__(self,shabba: str, **kwargs) ->None:
+        super().__init__(**kwargs)
+        self.shabba = shabba
+    
+
+    def order(self, order: str) -> None:
         print(f"{self.user_name} from your product {order!r} was sold")
             
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(user_name={self.user_name!r}, email={self.email!r}, password={self.password!r}, shabba={self.shabba!r})"
+
+
 class Buyer(User):
 
-    def __init__(self, user_name: str, email: str, password: str, phone: str) -> None:
-        super().__init__(user_name, email, password)
+    def __init__(self, phone: str, **kwargs) -> None:
+        super().__init__(**kwargs)
         self.phone = phone
 
 
@@ -52,16 +64,31 @@ class Buyer(User):
 
 
 
+class SelerAndBuyer(Seller, Buyer):
+    def __init__(self, score: str, **kwargs) -> None:
+
+        super().__init__(**kwargs)
+
+        self.score = score
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(user_name={self.user_name!r}, email={self.email!r}, password={self.password!r}, phone={self.phone!r}, shabba={self.shabba!r}, score={self.score!r})"
+
+
 
 
 
 john = User("John", "john@example.com", "John1234")
 
-seler_test = Seller("kate", "kate@example.com", "kate1234")
+seler_test = Seller(user_name="kate", email="kate@example.com", password="kate1234", shabba="123")
 
-seler_test2 = Seller("joli", "joli@example.com", "joli1234")
+seler_test2 = Seller(user_name="joli", email="joli@example.com", password="joli1234", shabba="123")
 
-buyer1 = Buyer("larry", "larry@example.com", "larry1234", "0934")
+buyer1 = Buyer(user_name="larry", email="larry@example.com", password="larry1234", phone="0934")
+
+buyer_and_seler = SelerAndBuyer(user_name="larry", email="larry@example.com", password="larry1234", phone="0934", shabba="65677", score="18")
 
 
-pprint(repr(buyer1.all_users))
+pprint(buyer1.all_users)
+
+buyer_and_seler.order(order="boock")
