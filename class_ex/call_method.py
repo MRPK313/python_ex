b740ca998e735efc03a6f3d4d3e60f5486136866
AttackThwarter <attackthwarter@gmail.com>
2024-12-11 11:33:54 +0330

    write calable objects with __call__

--- class_ex/call_method.py
+++ class_ex/call_method.py
@@ -1,2 +1,25 @@
 # calable objects with __call__ 
 
+from typing import Any
+
+
+class A:
+
+    def __init__(self, x) -> None:
+        print("init")
+        self.x=x
+
+    def __call__(self, y) -> None:
+        print("__call__")
+        self.y=y
+
+obj = A(2)
+print(obj.x)
+
+
+
+obj(6)
+
+print(obj.y)
+
+print(obj.__dict__)
